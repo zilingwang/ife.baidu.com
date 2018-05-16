2d62ef84f35e068e1308c3cf2fe3f00eace09b7b
zilingwang <244663586@qq.com>
2018-05-16 23:55:29 +0800

    [update] 学习parcel & webpack & san

--- webpack_san/src/main.js
+++ webpack_san/src/main.js
@@ -1,1 +1,15 @@
-console.log('Hello webpack4 & San');
+import san from 'san';
+import {router} from 'san-router';
+import San from './app.san';
+
+console.log('hello webpack San');
+
+// 这是控制路由，引入San组件  不要问我怎么知道，看官方demo学来的。这个东西坑了我好久
+router.add({
+    rule: '/',
+    Component: San,
+    target: '#app',
+});
+
+// 一定要记得启动
+router.start();
